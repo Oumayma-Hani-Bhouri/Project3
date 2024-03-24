60978ac279b2302b6877114b6004c5c8b6039297
Oumayma Hani <oumayma.hani278@gmail.com>
2024-03-24 07:11:26 +0100

    add edit bar and button

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,44 +1,42 @@
+const formLogin = document.querySelector(".loginform");
 
-const formLogin=document.querySelector(".loginform");
-
-formLogin.addEventListener("submit", async function(event) {
-
-event.preventDefault();
-// Récupération des valeurs des champs email et mot de passe
-const identifier= {
+formLogin.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  // Récupération des valeurs des champs email et mot de passe
+  const identifier = {
     email: event.target.querySelector("[name=email]").value,
-    password:event.target.querySelector("[name=password]").value,
-};
-const chargeUtile=JSON.stringify(identifier);
+    password: event.target.querySelector("[name=password]").value,
+  };
+  const chargeUtile = JSON.stringify(identifier);
 
-try {
-   const response = await fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: {
-       "Content-Type": "application/json"},
-    body: chargeUtile
-    })
-    console.log(response)
+  try {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: chargeUtile,
+    });
+    console.log(response);
 
- if (response.ok)
-  {
-   const data = await response.json();
-    console.log(data);
-    const token = data.token;
-	 console.log(token); //  la valeur du token dans la console
-	// Stocker le token
-	sessionStorage.setItem("token", token);
-	// Redirection vers la page d'accueil
-	window.location.href = "index.html";
-  } 
-// Affichage d'un message d'erreur
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      const token = data.token;
+      console.log(token); //  la valeur du token dans la console
+      // Stocker le token
+      localStorage.setItem("token", token);
+      // Redirection vers la page d'accueil
+      window.location.href = "index.html";
+    }
+    // Affichage d'un message d'erreur
     else {
-    const ErrorMsg= document.querySelector(".error-msg");
-    ErrorMsg.textContent="Mot de passe et/ou e-mail incorrect";
-         }
-         document.querySelector('#email').value=null;
-         document.querySelector('#password').value=null;
-     }
-
-catch  (error) { console.log ("une erreur est survenue",error)}
-})
+      const ErrorMsg = document.querySelector(".error-msg");
+      ErrorMsg.textContent = "Erreur dans l’identifiant ou le mot de passe";
+    }
+    document.querySelector("#email").value = null;
+    document.querySelector("#password").value = null;
+  } catch (error) {
+    console.log("une erreur est survenue", error);
+  }
+});
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,55 +1,91 @@
 // Appel a l API pour recuperer les projets
 
-const response= await fetch ('http://localhost:5678/api/works');
-const works= await response.json();
+const response = await fetch("http://localhost:5678/api/works");
+const works = await response.json();
 
 // Appel a l API pour recuperer les filtres
-const response2=await fetch ('http://localhost:5678/api/categories');
-const categories=await response2.json();
+const response2 = await fetch("http://localhost:5678/api/categories");
+const categories = await response2.json();
 
-// Recuperer les projets 
+// Recuperer les projets
 
-function displayWorks (works) {
-    
-for( const work of works ) {
-const galleryWorks=document.querySelector(".gallery");
-const project=document.createElement("project");
-const imgproject=document.createElement("img");
-imgproject.src=work.imageUrl;
-const caption=document.createElement("projectcaption");
-caption.innerText=work.title;
-galleryWorks.appendChild(project);
-project.appendChild(imgproject);
-project.appendChild(caption);
-}}
-displayWorks (works);
+function displayWorks(works) {
+  for (const work of works) {
+    const galleryWorks = document.querySelector(".gallery");
+    const project = document.createElement("project");
+    const imgproject = document.createElement("img");
+    imgproject.src = work.imageUrl;
+    const caption = document.createElement("projectcaption");
+    caption.innerText = work.title;
+    galleryWorks.appendChild(project);
+    project.appendChild(imgproject);
+    project.appendChild(caption);
+  }
+}
+displayWorks(works);
 
 // le filtre Tous
-const buttonfilterAll=document.createElement('button');
-buttonfilterAll.innerText="Tous";
-const filters=document.querySelector(".filters");
+const buttonfilterAll = document.createElement("button");
+buttonfilterAll.innerText = "Tous";
+const filters = document.querySelector(".filters");
 filters.appendChild(buttonfilterAll);
-buttonfilterAll.addEventListener("click",function() {
-document.querySelector(".gallery").innerHTML = "";
-displayWorks(works);
-})
+buttonfilterAll.addEventListener("click", function () {
+  document.querySelector(".gallery").innerHTML = "";
+  displayWorks(works);
+});
 // creation et activation des filtres
 
-function filterProjects (categories) {
-
-for (const category of categories){
-
-    const buttonfilter=document.createElement('button');
-    buttonfilter.innerText=category.name;
-    const filters=document.querySelector(".filters");
-   filters.appendChild(buttonfilter);
-    buttonfilter.addEventListener("click", function(){
-    const filteredProjects =works.filter(function(work) {
-        return work.categoryId===category.id;
+function filterProjects(categories) {
+  for (const category of categories) {
+    const buttonfilter = document.createElement("button");
+    buttonfilter.innerText = category.name;
+    const filters = document.querySelector(".filters");
+    filters.appendChild(buttonfilter);
+    buttonfilter.addEventListener("click", function () {
+      const filteredProjects = works.filter(function (work) {
+        return work.categoryId === category.id;
+      });
+      document.querySelector(".gallery").innerHTML = "";
+      displayWorks(filteredProjects);
     });
-    document.querySelector(".gallery").innerHTML= "";
-    displayWorks (filteredProjects)
-    })
-    }}
+  }
+}
+filterProjects(categories);
 
-   filterProjects (categories);
+// Passer au mode edition
+
+if (localStorage.getItem("token")) {
+  // modifier le text du lien login en logout
+  const login = document.querySelector("#logIn");
+  login.innerText = "logout";
+  console.log("Vous êtes connecté !");
+  LogInMode();
+  // fonction pour Supprimer le token et revenir vers la page de connexion
+  login.addEventListener("click", function () {
+    localStorage.removeItem("token");
+    window.location.href = "login.html";
+  });
+}
+
+function LogInMode() {
+  // supprimer la partie filtres
+  document.querySelector(".filters").style.display = "none";
+  // Ajout de la barre d edition
+  const body = document.querySelector("body");
+  body.style.paddingTop = "40px";
+  const headerEdit = document.createElement("div");
+  body.appendChild(headerEdit);
+  headerEdit.classList.add("header-edit");
+  const EditMode = document.createElement("div");
+  EditMode.classList.add("edit");
+  EditMode.innerHTML = ` <i class="fa-regular fa-pen-to-square"></i> <p>Mode édition</p> `;
+  headerEdit.appendChild(EditMode);
+
+  // Ajout du lien Modification
+  const TitlePortfolio = document.querySelector(".TitlePortfolio");
+  const TitleEdit = document.createElement("a");
+  TitleEdit.classList.add("editLink");
+  TitleEdit.href = "#";
+  TitleEdit.innerHTML = ` <i class="fa-regular fa-pen-to-square"></i> <p> modifier <p> `;
+  TitlePortfolio.appendChild(TitleEdit);
+}
